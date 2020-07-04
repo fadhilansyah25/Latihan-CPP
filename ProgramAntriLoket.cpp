8b023d95c1e40ed7a2356a06c6e99b4d1ae7aa1c
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-07-04 23:40:26 +0700

    menambahkan fungsi tambah antrian

--- ProgramAntriLoket.cpp
+++ ProgramAntriLoket.cpp
@@ -1,15 +1,59 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-struct node
+// pembuatan simpul 
+struct Node
 {
-    string nama;
-    int nomer_antrian;
-    struct node *next;
+    string Name;
+    int Queue_Number;
+    struct Node *next;
 };
 
+// fungsi menambah antrian
+void tambah_antrian(Node **tail, int nomer_antrian, string nama)
+{
+    Node *baru = new Node(), *bantu = new Node();
+    baru->Name = nama;
+    baru->Queue_Number = nomer_antrian;
+    if (*tail == NULL){
+        *tail = baru;
+    }else
+    {
+        bantu = *tail;
+        while (bantu->next != NULL)
+        bantu = bantu->next;
+        bantu->next = baru;
+    }
+}
 
+// fungsi utama
 main()
 {
+    int pilihan, nomer_antrian = 1;
+    string nama;
+    Node *tail; 
 
+    menu:
+    system("CLS");
+    cout<<"============================================="<<endl;
+    cout<<"Program Antri Loket Pembayaran Mahasiswa"<<endl;
+    cout<<"============================================="<<endl;
+    cout<<"1. Tambah Antrian"<<endl;
+    cout<<"2. Panggil Antrian"<<endl;
+    cout<<"3. Tampilkan Antrian Tersisa"<<endl;
+    cout<<"============================================="<<endl;
+    cout<<"masukan pilihan : ";cin>>pilihan;
+
+    switch (pilihan)
+    {
+    case 1:
+        system("CLS");
+        cout<<"Masukan nama : ";cin>>nama;
+        nomer_antrian++;
+        tambah_antrian(&tail, nomer_antrian, nama);
+        goto menu;
+        break;
+    default:
+        break;
+    }
 }
