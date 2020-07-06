17fa0eefe53299c8642f7b06b86d84be3c78df5d
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-07-06 11:02:53 +0700

    Updated ProgramAntriLoket.cpp V.1.0 Beta

--- ProgramAntriLoket.cpp
+++ ProgramAntriLoket.cpp
@@ -15,8 +15,10 @@ struct Node
 void tambah_antrian(Node **tail, int nomer_antrian)
 {
     Node *baru = new Node(), *bantu = new Node();
+    cout<<"*jangan input dengan spasi\n";
     cout<<"Masukan Nama Pengantri : ";cin>>baru->Name;
     baru->Queue_Number = nomer_antrian;
+    baru->next = NULL;
 
     if (*tail == NULL){
         *tail = baru;
@@ -29,29 +31,34 @@ void tambah_antrian(Node **tail, int nomer_antrian)
     }
 }
 
+// Fungsi mengeluarkan antrian atau memanggil antrian
 void panggil_antrian(Node **tail)
 {
-    Node *bantu = new Node();
+    Node *bantu;
     bantu = *tail;
     if(*tail == NULL){
         cout<<"Antrian Sudah habis ....."<<endl;
     }
     else if(bantu->next == NULL){
-        free(bantu);
+        cout<<"Antrian telah dipanggil\n";
+        cout<<"Antrian dengan Nama : "<<bantu->Name<<"\nDengan nomor Antrian : "<<bantu->Queue_Number<<endl
+        <<"\nTelah dipanggil";
+        delete bantu;
         *tail = NULL;
-        cout<<"Antrian telah dipanggil";
     }
     else{
+        cout<<"Antrian telah dipanggil\n";
+        cout<<"Antrian dengan Nama : "<<bantu->Name<<"\nDengan nomor Antrian : "<<bantu->Queue_Number<<endl
+        <<"\nTelah dipanggil";
         *tail = bantu->next;
-        free(bantu);
-        cout<<"Antrian telah dipanggil";
+        delete bantu;
     }
 }
 
 // fungsi mencetak sisa antrian
 void cetak_antrian(Node **tail)
 {
-    Node *bantu = new Node();
+    Node *bantu;
     if(*tail == NULL){
         cout<<"Antrian Sudah habis ....."<<endl;
     }else
@@ -68,21 +75,54 @@ void cetak_antrian(Node **tail)
     }
 }
 
+// fungsi melongkap antrian
+void panggil_Antrian_Acak(Node **tail)
+{
+    int longkap;
+    Node *bantu, *sebelum;
+    bantu = *tail;
+    cout<<"Masukan nomer antrian : ";cin>>longkap;
+
+    if(bantu != NULL && bantu->Queue_Number == longkap){
+        *tail = bantu->next;
+        cout<<bantu->Name<<" Antrian telah dipanggil\n";
+        delete bantu;
+    }
+    else if (bantu != NULL && bantu->Queue_Number != longkap)
+    {
+        while (bantu->Queue_Number != longkap)
+        {
+            sebelum = bantu;
+            bantu = bantu->next;
+        }
+
+        if(bantu == NULL){
+            return;
+        }
+        else
+        {
+            sebelum->next = bantu->next;
+            delete bantu;
+        }
+    }
+    
+}
+
 // fungsi utama
 main()
 {
     int pilihan, nomer_antrian = 1;
     Node *tail = NULL; 
-
     menu:
     system("cls");
     cout<<"============================================="<<endl;
-    cout<<"Program Antri Loket Pembayaran Mahasiswa"<<endl;
+    cout<<"Program Otomatis Nomor Antrian Loket Pembayaran Mahasiswa"<<endl;
     cout<<"============================================="<<endl;
     cout<<"1. Tambah Antrian"<<endl;
-    cout<<"2. Panggil Antrian"<<endl;
+    cout<<"2. Panggil Antrian Paling Lama menunggu"<<endl;
     cout<<"3. Tampilkan Antrian Tersisa"<<endl;
-    cout<<"4. Keluar dari program"<<endl;
+    cout<<"4. Panggil Antrian Secara Random"<<endl;
+    cout<<"5. Keluar dari program"<<endl;
     cout<<"============================================="<<endl;
     cout<<"masukan pilihan : ";cin>>pilihan;
 
@@ -105,6 +145,11 @@ main()
         getch();
         goto menu;
     case 4:
+        system("cls");
+        panggil_Antrian_Acak(&tail);
+        getch();
+        goto menu;
+    case 5:
         exit(0);
     default:
         goto menu;
